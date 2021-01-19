ce0d87a636fe585fd4ccd0f890f99692302795de
Nirupa <nirupasinha4@gmail.com>
2021-01-19 22:07:51 +0530

    component,function,module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//component name->external html file in body(access in body of root html file )
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'SMS';
+  name="nirupa sinha"
+  getName(){
+    return this.name;
+   // return "nirupa";
+  }
+  obj={
+    name:"nirupa sinha",
+    age:26
+  }
+  arr=["nirupa","rishi"]
+  a=300;
+  c=600;
+ names="nirupa"
+  getAlert(nam:Number)
+  {
+    alert(nam);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UsersModule } from './users/users.module';//because of command line import automatically 
 
-@NgModule({
-  declarations: [
-    AppComponent
+@NgModule({//ngModule is called directives
+  declarations: [//components declaration
+    AppComponent,
+    UserListComponent
+    //if we remove the declaration then it will not affect when we try to use or import module to another module it will take error
   ],
-  imports: [
+  imports: [//import one module to another module
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]//bootstrap means which component will use
 })
 export class AppModule { }
