5d966152e5649d2c6f5ddec4e4434308e71943d7
Nirupa <nirupasinha4@gmail.com>
2021-01-22 19:00:02 +0530

    till style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ArgumentOutOfRangeError } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',//component name->external html file in body(access in body of root html file )
@@ -9,7 +11,7 @@ export class AppComponent {
   title = 'SMS';
   name="nirupa sinha"
   getName(){
-    return this.name;
+   return this.name;
    // return "nirupa";
   }
   obj={
@@ -24,4 +26,57 @@ export class AppComponent {
   {
     alert(nam);
   }
+  //function call on event
+  myEvent(evt:any){
+    console.log(evt);
+    
+  }
+  currentVal="";
+  getVal(val:string){
+    console.log(val);
+    this.currentVal=val;
+  }
+  disableBOX=true;
+  enableBOX(){
+this.disableBOX=false;
+  }
+  show=false;
+  hide=false;
+  showBoolean="yes";
+  showColor="red";
+  color='red';
+  //For Loop
+  data=["Nirupa", "Sinha","rishi","ved","Sarvottam"]
+  dataObj=[
+    {
+      name:'Nirupa',
+      age:26,
+      email:"niru@gmail.com"
+    },
+    {
+      name:'Sarvottam',
+      age:29,
+      email:"niru@gmail.com"
+    },
+    {
+      name:'Rishi',
+      age:23,
+      email:"niru@gmail.com"
+    },
+    {
+      name:'Ved',
+      age:30,
+      email:""
+    }
+  ]
+  getUserValue(value:any){
+    console.log(value);
+    
+  }
+  //style binding
+  colorBinding="orange";
+  updateColor(){
+    this.colorBinding="blue"
+  };
+  err=false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UsersModule } from './users/users.module';//because of command line import automatically 
+import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap' ;
 
 @NgModule({//ngModule is called directives
   declarations: [//components declaration
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    HeaderComponent,
+    FooterComponent
+    
     //if we remove the declaration then it will not affect when we try to use or import module to another module it will take error
   ],
   imports: [//import one module to another module
     BrowserModule,
     AppRoutingModule,
-    UsersModule
+    UsersModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]//bootstrap means which component will use
